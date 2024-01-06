a6d70f8a38e4915fd119023379cb01e169dacc6a
kanjah <patokanjah@gmail.com>
2024-01-06 11:34:45 +0300

    installed bcrypt, hashed password, created user util i.e data>user.ts

--- actions/register.ts
+++ actions/register.ts
@@ -1,7 +1,9 @@
 "use server";
-
+import bcrypt from "bcrypt";
 import {  RegisterSchema } from "@/schema";
 import * as z from "zod";
+import { db } from "@/lib/db";
+import { getUserByEmail } from "@/data/user";
 
 
 
@@ -12,5 +14,26 @@ export const register = async (values: z.infer<typeof RegisterSchema>) => {
   if (!validatedFields.success) {
     return { error: "Invalid fields!" };
   }
-  return {success: "Email sent!"}
+
+  // hash the password
+  const { email, password, name } = validatedFields.data;
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  // check for existing user
+  const existingUser = await getUserByEmail(email);
+  //const existingUser = await db.user.findUnique({ where: { email } });
+  if (existingUser) {
+    return { error: "Email already in use!" };
+  }
+
+  await db.user.create({
+    data: {
+      name,
+      email,
+      password: hashedPassword,
+    },
+  });
+
+  //TODO: send verification token email
+  return {success: "User created successfully"}
 }
